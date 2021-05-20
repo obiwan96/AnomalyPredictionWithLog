189f1f40ff3ccc2cd987c3f215c1e9d749bb88fe
obiwan96 <obiwan96@kaist.ac.kr>
2021-05-21 01:57:40 +0900

    CRNN complete

--- Model.py
+++ Model.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Embedding, Dropout, Conv1D, GlobalMaxPooling1D, Dense, Concatenate, Flatten, Input
+from tensorflow.keras.layers import Embedding, Dropout, Conv1D, GlobalMaxPooling1D
+from tensorflow.keras.layers import Dense, Concatenate, Flatten, Input, LSTM, Reshape
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from keras.regularizers import l2
 from keras import backend as K
@@ -25,7 +26,7 @@ def f1_m(y_true, y_pred):
     recall = recall_m(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
-def new_model():
+def CNN():
     embed_size=100
     model_input=Input(shape=(None,1))
     #model=Sequential()
@@ -40,6 +41,28 @@ def new_model():
     z=Concatenate()(submodels)
     z=Dropout(0.5)(z)
     model_output=Dense(1,activation='sigmoid')(z)
+    model=Model(model_input, model_output)
+    model.summary()
+    model.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['acc',f1_m,precision_m, recall_m])
+    return model
+
+def CRNN():
+    #model_input=Input(batch_input_shape=(1,None,1))
+    model_input=Input(shape=(None,1))
+    submodels=[]
+    embed_size=100
+    for kw in (3,4,5):
+        conv=Conv1D(100, kernel_size=(kw*embed_size,), padding='valid', activation='relu', kernel_regularizer=l2(3),strides=embed_size)(model_input)
+        conv=GlobalMaxPooling1D()(conv)
+        #conv=Flatten()(conv)
+        submodels.append(conv)
+    z=Concatenate()(submodels)
+    z=Dropout(0.5)(z) # (None, 300)
+    z=Reshape(target_shape=((1,300)))(z)
+    z=LSTM(128, return_sequences=False,stateful=False)(z)
+
+    model_output=Dense(1,activation='sigmoid')(z)
+
     model=Model(model_input, model_output)
     model.summary()
     model.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['acc',f1_m,precision_m, recall_m])
